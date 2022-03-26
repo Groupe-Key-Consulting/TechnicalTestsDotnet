e098d48339dc5d1185338fa73a27abdf390f2c15
alex-toy <alexei.80@hotmail.fr>
2022-03-26 08:49:17 +0100

    Level One : added class Solution.

---
+++ TechnicalTests/TechnicalTests/LevelTwo/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechnicalTests.LevelTwo
+{
+    public class Solution
+    {
+        public static int Calculate(string operations)
+        {
+            return 1;
+        }
+    }
+}
